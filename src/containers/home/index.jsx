9215a95f73905c381fdc27029ae23e9fa6b8a289
DanielVertlib <danielvertlib@hotmail.ca>
2017-04-08 15:12:13 -0400

    Initial home page is setup, starting work on functionality

---
+++ src/containers/home/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './index.css'
+
+class HomeContainer extends Component {
+  render() {
+    return (
+      <div className="home-container">
+        <div className="home-header">Nusic</div>
+      </div>
+    )
+  }
+}
+
+export default HomeContainer
